a238dca880e871f36965a1fe99233fcb8257f9ed
Alejandro Cerezo <alce65@hotmail.es>
2025-05-06 21:34:36 +0200

    Update initial React component test and implementation

--- react-demo/src/components/counter.test.tsx
+++ react-demo/src/components/counter.test.tsx
@@ -1,5 +1,7 @@
-import { render, screen } from "@testing-library/react";
+// import { fireEvent } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
+// import userEvent from '@testing-library/user-event'
 import { Counter } from "./counter";
 
 describe("Sample", () => {
@@ -15,4 +17,24 @@ describe("Sample", () => {
         expect(element).toBeInTheDocument();
     });
 
+    test("it should show 1 when clicked", async () => {
+        render(<Counter />);
+        const eButton = screen.getByRole("button", { name: /click me/i });
+
+        // Método click de los nodos del DOM
+
+        act(() => {
+            eButton.click();
+        });
+
+        // Método click de fireEvent
+        // fireEvent.click(eButton);
+
+        // librería UserEvent
+        // await userEvent.click(eButton);
+
+        const element = screen.getByText(/clicked 1/i);
+        // const element = screen.findByText(/clicked 1/i);
+        expect(element).toBeInTheDocument();
+    });
 });
--- react-demo/src/components/counter.tsx
+++ react-demo/src/components/counter.tsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 export const Counter = () => {
-    const [ count ] = useState(0);
+    const [ count, setCount ] = useState(0);
 
     return (
         <div>
            <h2>Count</h2>
            <p>You clicked {count} times</p>
-            {/* //
-            // <button onClick={() => setCount(count + 1)}>
-            //     Click me
-            // </button> */}
+            {
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>}
         </div>
     );
 }
