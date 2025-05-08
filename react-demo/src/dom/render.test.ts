daccdc2b2f456ecbd40b68f0ea8e5c3cd858a39b
Alejandro Cerezo <alce65@hotmail.es>
2025-05-08 10:49:06 +0200

    Update pure TS render tests

--- react-demo/src/dom/render.test.ts
+++ react-demo/src/dom/render.test.ts
@@ -1,21 +1,31 @@
 import { screen } from "@testing-library/dom";
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
 import { render } from "./render";
 
-
 describe("function render", () => {
+    beforeEach(() => {
+        document.body.innerHTML = "";
+    });
     test("should insert HTML into the body (test with DOM)", () => {
         // Act
         render();
         // Assert
-        const pElement = document.querySelector("p") as HTMLParagraphElement
+        const pElement = document.querySelector("p") as HTMLParagraphElement;
         expect(pElement.textContent).toBe("Sample");
     });
-    test.only("should insert HTML into the body (test with TL)", () => {
+    test("should insert HTML into the body (test with TL)", () => {
         // Act
         render();
         // Assert
-        const pElement = screen.getByText("Sample")
+        const pElement = screen.getByText("Sample");
         expect(pElement).toBeInTheDocument();
+    });
+
+    test("should not insert any if the selector is invalid", () => {
+        // Act
+        render("invalid-selector");
+        // Assert
+        const pElement = screen.queryByText("Sample");
+        expect(pElement).toBe(null);
     });
 });
